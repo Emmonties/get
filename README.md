0ec7880878ec291529251320b08a6d4e048328f6
Emmonties <lete0429@gmail.com>
2023-03-26 17:18:40 +0800

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# dict-get
+
+Use "." to get the value of a JSON object. Just like JavaScripthat gets the value of an object
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from dict_get import get
+
+
+my_dict = {"name": {"first": "John", "last": "Doe"}, "age": 30}
+
+print(get(my_dict,'a.b'))
